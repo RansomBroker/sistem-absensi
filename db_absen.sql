09f247c9a6e92f4233fe70fb9f7b3411b35ed7bb
RansomBroker <yadisoke@gmail.com>
2023-03-27 07:20:54 +0700

    update db

--- db_absen.sql
+++ db_absen.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 19/03/2023 12:46:28
+ Date: 27/03/2023 07:20:26
 */
 
 SET NAMES utf8mb4;
@@ -22,26 +22,21 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `jadwal_presensi`;
 CREATE TABLE `jadwal_presensi` (
-  `id_mata_kuliah` bigint(20) NOT NULL AUTO_INCREMENT,
-  `id` bigint(20) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `nama` varchar(255) NOT NULL,
   `jam_masuk` time NOT NULL,
   `jam_keluar` time NOT NULL,
   `tgl_absen` date NOT NULL,
-  PRIMARY KEY (`id_mata_kuliah`),
+  `waktu_dispensasi` int(255) NOT NULL,
   KEY `id` (`id`),
-  CONSTRAINT `id_mata_kuliah_foreign` FOREIGN KEY (`id_mata_kuliah`) REFERENCES `mata_kuliah` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `jam_masuk` (`jam_masuk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Table structure for mata_kuliah
+-- Records of jadwal_presensi
 -- ----------------------------
-DROP TABLE IF EXISTS `mata_kuliah`;
-CREATE TABLE `mata_kuliah` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `nama` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+BEGIN;
+COMMIT;
 
 -- ----------------------------
 -- Table structure for presensi_mahasiswa
@@ -60,9 +55,14 @@ CREATE TABLE `presensi_mahasiswa` (
   PRIMARY KEY (`id`),
   KEY `id_mahasiswa_foreign` (`id_mahasiswa`),
   KEY `id_jadwal_presensi_foreign` (`id_jadwal_presensi`),
-  CONSTRAINT `id_jadwal_presensi_foreign` FOREIGN KEY (`id_jadwal_presensi`) REFERENCES `jadwal_presensi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `id_mahasiswa_foreign` FOREIGN KEY (`id_mahasiswa`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of presensi_mahasiswa
+-- ----------------------------
+BEGIN;
+COMMIT;
 
 -- ----------------------------
 -- Table structure for users
@@ -81,7 +81,16 @@ CREATE TABLE `users` (
   `angkata` int(255) DEFAULT NULL COMMENT 'mahasiswa',
   `moto_hidup` varchar(255) DEFAULT NULL COMMENT 'mahasiswa',
   `kemampuan_pribadi` varchar(255) DEFAULT NULL COMMENT 'mahasiswa',
+  `password` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+INSERT INTO `users` VALUES (1, NULL, 'admin', NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, '$2y$10$QHwST/QZ1JzlyqLY5dER6e/ZQCsMO24jmsjWpITodeHPekiEYA5ka', 'admin');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
