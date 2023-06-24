5ad8d867788081d42ce249d7f22898d420af0dfe
RansomBroker <yadisoke@gmail.com>
2023-06-24 11:12:43 +0700

    menambahkan login sebagai

--- function.php
+++ function.php
@@ -7,25 +7,37 @@ function login($form){
     global $connection;
 
 
-    	$username = $form['username'];
+    $username = $form['username'];
 	$password = $form['password'];
 	$check_username_query = "SELECT * FROM users WHERE username = '$username'";
 	$check_username_result = mysqli_query($connection, $check_username_query);
 	$username_in_db = mysqli_fetch_assoc($check_username_result);
-	
-	if ($username_in_db != null) {
+	$role = $form['role'];
+	if ($username_in_db !=   null) {
 		if ($username== $username_in_db['username']){
 			if (password_verify($password, $username_in_db['password'])){
-			  $_SESSION['login']=true;
-			  $_SESSION['id']=$username_in_db ['id'];
-			  $_SESSION['username']=$username_in_db ['username'];
-			  $_SESSION['role']=$username_in_db['role']; 
-			  $_SESSION['foto']=$username_in_db['img'];
-			  $_SESSION['nama'] = $username_in_db['nama'];
+                if (($role != $username_in_db['role']) && ($username_in_db['role'] == 3)) {
+                    set_flash_message('login_failed','Maaf anda bukan Admin/Dosen');  
+                    return;    
+                }
+                if (($role != $username_in_db['role']) && ($username_in_db['role'] == 1)) {
+                    set_flash_message('login_failed','Maaf anda bukan Dosen/Mahasiswa');      
+                    return;
+                }
+                if (($role != $username_in_db['role']) && ($username_in_db['role'] == 2)) {
+                    set_flash_message('login_failed','Maaf anda bukan Admin/Mahasiswa');      
+                    return;
+                }
+                $_SESSION['login']=true;
+                $_SESSION['id']=$username_in_db ['id'];
+                $_SESSION['username']=$username_in_db ['username'];
+                $_SESSION['role']=$username_in_db['role']; 
+                $_SESSION['foto']=$username_in_db['img'];
+                $_SESSION['nama'] = $username_in_db['nama'];
 				return redirect('index.php');
 			} else{
-			  set_flash_message('login_failed','Username atau password salah ');
-		   }
+			    set_flash_message('login_failed','Username atau password salah ');
+		    }
 		}else{
 			set_flash_message('login_failed','Username atau password tidak ditemukan');
 		}
--- login.php
+++ login.php
@@ -12,52 +12,64 @@
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php include "head.php"?>
 
-	<title>Login</title>
+    <title>Login</title>
 
     <?php include "css.php"?>
 </head>
-<body class="bg-warning bg-opacity-75 flex-column d-flex justify-content-center align-items-center vh-100">
-	<div class="flex-row justify-content-center">
-		<div class="d-flex justify-content-center align-items-center">
-			<img src="img/<?= ambil_logo_auth()['img']?>" width="180" class="  rounded-circle mb-2" alt="logo">
-		</div>
-		<h4 class="text-center text-dark">Validasi Kehadiran Mahasiswa</h4>
-	</div>
-	<div class="mx-lg-3 mx-1 my-2 py-lg-3 col-lg-3 card rounded login  ">
-		<div class="card-body py-0 px-3">
-			<h4 class="text-center">Login</h4>
-			<?php if(get_flash_name('login_failed') != ""):?>
-                    <div class="alert alert-danger" role="alert">
-                         <?= get_flash_message('login_failed')?>
-                    </div>	
-                <?php endif;?>
-			 <?php if(get_flash_name('reset_success') != ""):?>
-                    <div class="alert alert-success" role="alert">
-                         <?= get_flash_message('reset_success')?>
-                    </div>	
-                <?php endif;?>
-			<form class="" method="POST" action="">
-				<div class="d-flex  justify-content-center ">
-					<div class=" row">
-						<div class=" col-lg-12 mb-3 px-0 " >
-							<label class="form-label" for="username">Username</label>
-							<input type="text" id="username" name="username" class="form-control">
 
-						</div>
-						<div class="col-lg-12 mb-3 px-0" >
-							<label class="form-label" for="password">Password</label>
-							<input class="form-control" type="password" id="password" name="password">
-						</div>
-						<button name="login" class="btn btn-primary col-lg-12 mb-2" type="submit">Login</button>
-						<a href="reset-password.php" class=" mb-1"><u> Reset Password?</u></a>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-	<?php include "js.php"?>
+<body class="bg-warning bg-opacity-75 flex-column d-flex justify-content-center align-items-center vh-100">
+    <div class="flex-row justify-content-center">
+        <div class="d-flex justify-content-center align-items-center">
+            <img src="img/<?= ambil_logo_auth()['img']?>" width="180" class="  rounded-circle mb-2" alt="logo">
+        </div>
+        <h4 class="text-center text-dark">Validasi Kehadiran Mahasiswa</h4>
+    </div>
+    <div class="mx-lg-3 mx-1 my-2 py-lg-3 col-lg-3 card rounded login  ">
+        <div class="card-body py-0 px-3">
+            <h4 class="text-center">Login</h4>
+            <?php if(get_flash_name('login_failed') != ""):?>
+            <div class="alert alert-danger" role="alert">
+                <?= get_flash_message('login_failed')?>
+            </div>
+            <?php endif;?>
+            <?php if(get_flash_name('reset_success') != ""):?>
+            <div class="alert alert-success" role="alert">
+                <?= get_flash_message('reset_success')?>
+            </div>
+            <?php endif;?>
+            <form class="" method="POST" action="">
+                <div class="d-flex  justify-content-center ">
+                    <div class=" row">
+                        <div class=" col-lg-12 mb-3 px-0 ">
+                            <label class="form-label" for="username">Username</label>
+                            <input type="text" id="username" name="username" class="form-control">
+
+                        </div>
+                        <div class="col-lg-12 mb-3 px-0">
+                            <label class="form-label" for="password">Password</label>
+                            <input class="form-control" type="password" id="password" name="password">
+                        </div>
+                        <div class="col-lg-12 mb-3 px-0">
+                            <label for="role" class="form-label">Login Sebagai</label>
+                            <select name="role" class="form-control" id="role" required>
+                                <option value="" selected>-- Login Sebagai --</option>
+                                <option value="1">Admin</option>
+                                <option value="2">Dosen</option>
+                                <option value="3">Mahasiswa</option>
+                            </select>
+                        </div>
+                        <button name="login" class="btn btn-primary col-lg-12 mb-2" type="submit">Login</button>
+                        <a href="reset-password.php" class=" mb-1"><u> Reset Password?</u></a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php include "js.php"?>
 </body>
+
 </html>
